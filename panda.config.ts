6eb64ad7fa7b723ee3dd69a1781e6056dcda1f41
Sybren W <sybren.willemot@gmail.com>
2024-03-03 13:18:03 +0100

    feat: use semantic tokens

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,23 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      semanticTokens: {
+        colors: {
+          airbnb: {
+            accent: { value: "{colors.pink.800}", description: "The Airbnb light accent color" },
+            accentLight: { value: "{colors.pink.200}", description: "The Airbnb light accent color" },
+          },
+          button: {
+            black: { value: "#222222", description: "The black color to use in buttons"},
+            bg: {
+              DEFAULT: { value: "white", description: "The default background color of the button" },
+              state: { value: "#F7F7F7", description: "The background color of the button when in a state"},
+            }
+          }
+        }
+      }
+    },
   },
 
   // The output directory for your css system
